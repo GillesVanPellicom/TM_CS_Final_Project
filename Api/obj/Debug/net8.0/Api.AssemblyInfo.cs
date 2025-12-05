4e953b454c16d9e36820429f69752587cbd618b0
Gilles Van pellicom <gilles.van.pellicom1@gmail.com>
2025-12-05 22:04:47 +0100

    Working emulation build

--- Api/obj/Debug/net8.0/Api.AssemblyInfo.cs
+++ Api/obj/Debug/net8.0/Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea3e057112b92fc91a2cd448271413ae67854bbd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+92a3b0553c55891685d0be452dc7ff40eb1b9267")]
 [assembly: System.Reflection.AssemblyProductAttribute("Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
