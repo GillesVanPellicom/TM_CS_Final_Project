4f885ac7468b2fc798bc94004412e406f1862394
Gilles Van pellicom <gilles.van.pellicom1@gmail.com>
2025-12-09 18:09:40 +0100

    Working mac catalyst version

--- Api/obj/Debug/net8.0/Api.AssemblyInfo.cs
+++ Api/obj/Debug/net8.0/Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1dabcc1a1f596a52924627d772238a251cd69df3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ed7ccd94eafb831a7bde9ba6b6f5cbfa1bbc7c1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
