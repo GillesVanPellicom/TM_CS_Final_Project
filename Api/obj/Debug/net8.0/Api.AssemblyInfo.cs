a03b4770ac65792964f5c7ae0457b913677df29a
Gilles Van pellicom <gilles.van.pellicom1@gmail.com>
2025-12-10 16:08:13 +0100

    Fixed misc. Android bugs

--- Api/obj/Debug/net8.0/Api.AssemblyInfo.cs
+++ Api/obj/Debug/net8.0/Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ed7ccd94eafb831a7bde9ba6b6f5cbfa1bbc7c1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f885ac7468b2fc798bc94004412e406f1862394")]
 [assembly: System.Reflection.AssemblyProductAttribute("Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
